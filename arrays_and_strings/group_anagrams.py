980ab80fe0acbf4ff318b991998b57a3f22bd303
Kritanu <kritanu82@gmail.com>
2022-05-04 16:35:52 +0530

    group ana

---
+++ arrays_and_strings/group_anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        hashmap = defaultdict(list)
+        
+        for each_str in strs:
+            count = [0] * 26
+            for charac in each_str:
+                count[ord(charac) - ord('a')] += 1
+            
+            hashmap[tuple(count)].append(each_str)
+            
+        return hashmap.values()
+                
+        #Complexity O(n * m)
