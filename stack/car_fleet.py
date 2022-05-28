5b87e090dba7d66157109c584dd75b0cd35947ba
Kritanu <kritanu82@gmail.com>
2022-05-28 14:46:03 +0530

    fixed mac permissions

---
+++ stack/car_fleet.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_speed = [[p, s] for p, s in zip(position, speed)]
+        stack = []
+        
+        for p, s in sorted(pos_speed)[::-1]:
+            t = (target - p)/ s
+            stack.append(t)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+    
+    #Complexity O(n), space O(n)
+    #Brute O(n^2)
